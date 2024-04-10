7e259fb44b0289cb453c85caacec4b8f4b3a780b
SabitCanTurunc <turuncsc@gmail.com>
2024-04-10 06:14:01 +0300

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summerizer-AI"
@@ -11,20 +10,17 @@ SRC_REPO = "textSummerizer"
 AUTHOR_EMAIL = "turuncsc@gmail.com"
 
 setuptools.setup(
-
     name=SRC_REPO,
     version=__version__,
-    author=AUTHOR_USER_NAME,
-    email=AUTHOR_EMAIL
+    author=AUTHOR_USER_NAME,    
+    author_email=AUTHOR_EMAIL,  
     description="A small python package for NLP app",
-    Long_description=long_description,
-    Long_description_content="text/markdown",
-
+    long_description=long_description,  
+    long_description_content_type="text/markdown",  
     url="https://github.com/SabitCanTurunc/Text-Summerizer-AI",
     project_urls={
         "Bug Tracker": "https://github.com/SabitCanTurunc/Text-Summerizer-AI/issues",
     },
     package_dir={'': 'src'},
-
     packages=setuptools.find_packages(where='src'),
 )
